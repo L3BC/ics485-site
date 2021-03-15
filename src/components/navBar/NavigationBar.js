c95736b3bc6a3cce1c0c3b17c734bdf85d04a79f
gbfrancisco <grahambr@hawaii.edu>
2021-03-14 21:29:01 -1000

    Fixed and Adjusted NavBar

--- src/components/navBar/NavigationBar.js
+++ src/components/navBar/NavigationBar.js
@@ -1,18 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 const NavigationBar = (props) => {
+    const styles = {
+        linkWidth: {
+            marginLeft: 3,
+            marginRight: 3
+        },
+        buttons: {
+            backgroundColor: '#000000',
+            border: 0
+        }
+    }
+
+    const [activeButton, setActiveButton] = useState(0);
+
+    useEffect(() => {
+        const parsedActiveButton = Number(localStorage.getItem("activeButton") || 0);
+        setActiveButton(parsedActiveButton);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("activeButton", activeButton);
+    }, [activeButton])
+
     return (
         <Navbar className='navBar' expand="lg">
             <Navbar.Brand className='navText'>ICS 485</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link as={Link} className='navText' to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} className='navText' to="/project">Project</Nav.Link>
-                    <Nav.Link as={Link} className='navText' to="/members">Members</Nav.Link>
+                <Nav className="mr-auto" style={{marginTop: 8, marginBottom: 8}}>
+                    <Nav.Item
+                        style={styles.linkWidth}
+
+                    >
+                        <Button
+                            as={Link} className='navText'
+                            to="/"
+                            style={{
+                                ...styles.buttons,
+                                fontWeight: activeButton == 0 ? 'bold' : null,
+                            }}
+                            onClick={() => setActiveButton(0)}
+                        >
+                            Home
+                        </Button>
+                    </Nav.Item>
+                    <Nav.Item
+                        style={styles.linkWidth}
+                    >
+                        <Button
+                            as={Link} className='navText'
+                            to="/project"
+                            style={{
+                                ...styles.buttons,
+                                fontWeight: activeButton == 1 ? 'bold' : null
+                            }}
+                            onClick={() => setActiveButton(1)}
+                        >
+                            Project
+                        </Button>
+                    </Nav.Item>
+                    <Nav.Item
+                        style={styles.linkWidth}
+                    >
+                        <Button
+                            as={Link} className='navText'
+                            to="/members"
+                            style={{
+                                ...styles.buttons,
+                                fontWeight: activeButton == 2 ? 'bold' : null
+                            }}
+                            onClick={() => setActiveButton(2)}
+                        >
+                            Members
+                        </Button>
+                    </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
