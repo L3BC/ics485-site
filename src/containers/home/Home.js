8bcd7097623d83138bec5dec3685d39dac9a040b
gbfrancisco <grahambr@hawaii.edu>
2021-05-07 22:55:20 -1000

    homepage

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -17,10 +17,35 @@ const Home = (props) => {
             fontSize: '36px',
             textAlign: 'center'
         },
-        homeFeaturesText: {
+        homeDescriptionTitle: {
+            marginBottom: 20,
+            color: '#000000',
+            fontWeight: 'bold',
+            fontSize: '36px',
+            textAlign: 'center',
+        },
+        homeDescriptionBody: {
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: '#000000',
+            textAlign: 'justify'
+        },
+        homeFeaturesTitle: {
+            marginBottom: 20,
             color: '#ffffff',
             fontWeight: 'bold',
             fontSize: '36px',
+            textAlign: 'center',
+        },
+        homeFeaturesSubTitle: { 
+            marginTop: 20,
+            color: '#ffffff',
+            textAlign: 'center'
+        },
+        homeFeaturesBody: {
+            marginTop: 20,
+            fontSize: 18,
+            color: '#ffffff',
             textAlign: 'center'
         },
         divider: {
@@ -49,19 +74,45 @@ const Home = (props) => {
                     </Row>
                 </Container>
             </div>
-            <div className="homeFeatures">
-                <Container fluid>
+            <div className="homeDescription">
+                <Container>
                     <Row>
                         <Col>
-                            <h1 style={styles.homeFeaturesText}>Features</h1>
+                            <h1 style={styles.homeDescriptionTitle}>Our Game</h1>
+                            <p style={styles.homeDescriptionBody}>
+                                Breaking Alpha is an FPS game made in Unity3D. It follows the conventional WASD movement
+                                system employed by other FPS games, with sprinting but without jumping mechanic. It also 
+                                contains an inventory system that allows the player to pick up item in order to interact 
+                                with other objects. In order to achieve the goal of escaping the facility, the player must 
+                                use time-travelling and time-slowing powers to solve puzzles along the way.
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="homeFeatures">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 style={styles.homeFeaturesTitle}>Features</h1>
                         </Col>
                     </Row>
                     <Row>
                         <Col>
-                            <h3 style={{ color: 'white', textAlign: 'center' }}>Time Travel</h3>
+                            <VideoFrame videoLink={`pERsblOacis`} />
+                            <h3 style={styles.homeFeaturesSubTitle}>Time Travel</h3>
+                            <p style={styles.homeFeaturesBody}>
+                                Move back and forth between past and present to navigate your way through obstacles.
+                                Some paths are not traversable normally, so use your powers to find circumvent them.
+                            </p>
                         </Col>
                         <Col>
-                            <h3 style={{ color: 'white', textAlign: 'center' }}>Slow Time</h3>
+                            <VideoFrame videoLink={`LhEfVZwdBxk`} />
+                            <h3 style={styles.homeFeaturesSubTitle}>Slow Time</h3>
+                            <p style={styles.homeFeaturesBody}>
+                                Some obstacles are too fast to go past through, and some enemies will hunt you non-stop.
+                                Give yourself enough time to react to these obstacles, by slowing down time itself.
+                            </p>
                         </Col>
                     </Row>
                 </Container>
